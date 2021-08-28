953b5e0be11cd4f8b79ea8a48ccf925bda7c298e
Timothy Do <timothd4@uci.edu>
2021-08-28 12:32:26 -0700

    Added Code for the Scouts

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+# Add More Functionality Like Asking for User Info for Req. 5a.
+a = 100
+b = 150
+c = a - b
+print(c)
---
+++ debug.py
@@ -0,0 +1,13 @@
+# Want to Print I am testing my abc(s) to successfully debug
+def debugThis():
+    a = "I am " + str(100) 
+    b = "testing my " + 'a'
+    c = "bc" + '(s)'
+    result = a + b + c
+    print("Result is " + result + ", Program is Successful")
+
+debugThis()
+#try:
+   #debugThis()
+#except TypeError:
+    #print("Program is not successful")
