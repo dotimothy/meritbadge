41ea449abc0cc5e726682918e5592d17522a22ec
Timothy Do <timothd4@uci.edu>
2021-08-28 23:28:49 -0700

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,12 @@
+# Author: Timothy Do
+
 # Add More Functionality Like Asking for User Info for Req. 5a.
+
+# Variable and Math Example
 a = 100
 b = 150
 c = a - b
 print(c)
+
+#Printing HelloWorld 
+print("Hello World")
